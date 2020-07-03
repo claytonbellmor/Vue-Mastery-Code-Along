d66bebb04e2def0cbe35abe50d6381ae261ce8b2
Clay <claytonbellmor@gmail.com>
2020-07-03 17:18:01 -0400

    Update EventService.js
    Added NProgress Axios Interceptors to start and end progress bar calling JSON API.
    
    Loaded json-server with 1500 ms delay to simulate slow server response:
    'json-server -d 1500 db.json'

--- Next Level Vue Lesson One/src/services/EventService.js
+++ Next Level Vue Lesson One/src/services/EventService.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
+import nprogress from 'nprogress'
+import nProgress from 'nprogress'
 
+// Single Axios Instance for App
 const apiClient = axios.create({
   baseURL: `http://localhost:3000`,
   withCredentials: false, // This is the default
@@ -9,6 +12,21 @@ const apiClient = axios.create({
   }
 })
 
+// Axios Interceptor
+// When request is about to go out, start the progress bar
+apiClient.interceptors.request.use(config => {
+  nProgress.start()
+  return config
+})
+
+//Axios Interceptor
+//When response returns, finish the progress bar
+apiClient.interceptors.response.use(response => {
+  nProgress.done()
+  return response
+})
+
+// Each API Call
 export default {
   getEvents(perPage, page) {
     return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
