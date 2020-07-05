244e787017e3ea42faffd4a399c0c658e2fd1eaf
Clay <claytonbellmor@gmail.com>
2020-07-04 23:20:43 -0400

    Updated router.js
    Some basic Git commands are:
    ```
    git status
    git add
    git commit
    ```

---
+++ Next Level Vue Lesson One/src/views/NetworkIssue.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Uh-oh!</h1>
+    <p>It looks like you're experiencing some network issues, please click to go back to the homepage.</p>
+    <router-link :to=" { name: 'event-list' } ">Homepage</router-link>
+  </div>
+</template>
