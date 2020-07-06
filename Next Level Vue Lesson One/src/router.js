326f321fdff87b88503795ac69d8e8e085cfe317
Clay <claytonbellmor@gmail.com>
2020-07-06 10:38:42 -0400

    Added 404 Routing for Slow API Network
    Created new NetworkIssue.vue component in the view directory.
    
    // router.js
    1. imported new component into router.js
    2 added path called network-issue
    3. added 'error' to our catch to receive the error that is thrown so we can inspect the type of error.
    4. We check if the error.response exists, and only if the error.response.status is 404 do we want to show the 404 component, otherwise, we redirect to the network-issue component.
    
    // EventService.js
    1. We added a timeout to the axios instance

--- Next Level Vue Lesson One/src/router.js
+++ Next Level Vue Lesson One/src/router.js
@@ -4,6 +4,7 @@ import EventCreate from './views/EventCreate.vue'
 import EventList from './views/EventList.vue'
 import EventShow from './views/EventShow.vue'
 import NotFound from './views/NotFound.vue'
+import NetworkIssue from './views/NetworkIssue.vue'
 import NProgress from 'nprogress'
 import store from '@/store/store'
 
@@ -35,13 +36,25 @@ const router = new Router({
             routeTo.params.event = event
             next()
           })
-          .catch(() => next({ name: '404', params: { resource: 'event' } }))
+          .catch(error => {
+            if (error.response && error.response.status == 404) {
+              next({ name: '404', params: { resource: 'event' } })
+            } else {
+              next({ name: 'network-issue' })
+            }
+          })
       }
     },
     {
       path: '/404',
       name: '404',
       component: NotFound,
+      props: true
+    },
+    {
+      path: '/network-issue',
+      name: 'network-issue',
+      component: NetworkIssue,
       props: true
     },
     {
