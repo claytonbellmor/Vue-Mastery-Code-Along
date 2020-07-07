808c96730ba4337cb78b209c455ddead895bc263
Clay <claytonbellmor@gmail.com>
2020-07-07 13:11:21 -0400

    Finish form validation
    BaseInput.vue
    Conflicting actions; v-on $listeners and @input updateValue conflict
    Compromise created by creating a new computed property that combine $listeners and updatevalue into one object in a return statement
    JS rules that the lowest value ("input: this.updateVlue") has precendence.
    We also remove the '$' from the inline v-on option.
    
    EventCreate.vue
    Added validation fields and error messages and classes to all form inputs.
    Each input adds a touch() call when @blur is triggered.
    
    Inside the methods property, we moved the submit action into a function that checks the form to make sure everything is valid before submitting to db.json by checking to make sure everything is dirty ("this.$v.$touch()") and there are no invalid fields.

--- Next Level Vue Lesson One/src/views/EventCreate.vue
+++ Next Level Vue Lesson One/src/views/EventCreate.vue
@@ -2,10 +2,39 @@
   <div>
     <h1>Create an Event</h1>
     <form @submit.prevent="createEvent">
-      <BaseSelect label="Select a category" :options="categories" v-model="event.category" />
+      <!-- Category -->
+
+      <BaseSelect
+        label="Select a category"
+        :options="categories"
+        v-model="event.category"
+        :class="{error: $v.event.category.$error}"
+        @blur="$v.event.category.$touch()"
+      />
+
+      <template v-if="$v.event.category.$error">
+        <p v-if="!$v.event.category.required" class="errorMessage">Category is required.</p>
+      </template>
+
       <h3>Name & describe your event</h3>
 
-      <BaseInput label="Title" v-model="event.title" type="text" placeholder="Title" class="field" />
+      <!-- Title -->
+
+      <BaseInput
+        label="Title"
+        v-model="event.title"
+        type="text"
+        placeholder="Title"
+        class="field"
+        :class="{ error: $v.event.title.$error }"
+        @blur="$v.event.title.$touch()"
+      />
+
+      <template v-if="$v.event.title.$error">
+        <p v-if="!$v.event.title.required" class="errorMessage">Title is required.</p>
+      </template>
+
+      <!-- Description -->
 
       <BaseInput
         label="Description"
@@ -13,7 +42,15 @@
         type="text"
         placeholder="Description"
         class="field"
+        :class="{ error: $v.event.description.$error }"
+        @blur="$v.event.description.$touch()"
       />
+
+      <template v-if="$v.event.description.$error">
+        <p v-if="!$v.event.description.required" class="errorMessage">Description is required.</p>
+      </template>
+
+      <!-- Location -->
 
       <h3>Where is your event?</h3>
       <BaseInput
@@ -22,25 +59,59 @@
         type="text"
         placeholder="Location"
         class="field"
+        :class="{ error: $v.event.location.$error }"
+        @blur="$v.event.location.touch()"
       />
+
+      <template v-if="$v.event.location.$error">
+        <p v-if="!$v.event.location.required" class="errorMessage">Location is required.</p>
+      </template>
+
+      <!-- Date -->
 
       <h3>When is your event?</h3>
 
       <div class="field">
         <label>Date</label>
-        <datepicker v-model="event.date" placeholder="Select a date" />
+        <datepicker
+          v-model="event.date"
+          placeholder="Select a date"
+          :wrapper-class="{ error: $v.event.date.$error}"
+          clear-button="true"
+          @opened="$v.event.date.$touch()"
+        />
       </div>
 
-      <BaseSelect label="Select a time" :options="times" v-model="event.time" class="field" />
+      <template v-if="$v.event.date.$error">
+        <p v-if="!$v.event.date.required" class="errorMessage">Date is required.</p>
+      </template>
 
-      <!-- <input type="submit" class="button -fill-gradient" value="Submit" /> -->
-      <BaseButton type="submit" buttonClass="-fill-gradient">Submit</BaseButton>
+      <!-- Time -->
+
+      <BaseSelect
+        label="Select a time"
+        :options="times"
+        v-model="event.time"
+        :class="{ error: $v.event.time.$error }"
+        class="field"
+        @blur="$v.event.time.$touch()"
+      />
+
+      <template v-if="$v.event.time.$error">
+        <p v-if="!$v.event.time.required" class="errorMessage">Time is required.</p>
+      </template>
+
+      <!-- Submit -->
+
+      <BaseButton type="submit" buttonClass="-fill-gradient" :disabled="$v.$anyError">Submit</BaseButton>
+      <p v-if="$v.$anyError" class="errorMessage">Please fill out the required fields.</p>
     </form>
   </div>
 </template>
 
 
 <script>
+import { required } from 'vuelidate/lib/validators'
 import Datepicker from 'vuejs-datepicker'
 import NProgress from 'nprogress'
 
@@ -59,21 +130,34 @@ export default {
       event: this.createFreshEventObject()
     }
   },
+  validations: {
+    event: {
+      category: { required },
+      title: { required },
+      description: { required },
+      location: { required },
+      date: { required },
+      time: { required }
+    }
+  },
   methods: {
     createEvent() {
-      NProgress.start()
-      this.$store
-        .dispatch('event/createEvent', this.event)
-        .then(() => {
-          this.$router.push({
-            name: 'event-show',
-            params: { id: this.event.id }
+      this.$v.$touch()
+      if (!this.$v.$invalid) {
+        NProgress.start()
+        this.$store
+          .dispatch('event/createEvent', this.event)
+          .then(() => {
+            this.$router.push({
+              name: 'event-show',
+              params: { id: this.event.id }
+            })
+            this.event = this.createFreshEventObject()
           })
-          this.event = this.createFreshEventObject()
-        })
-        .catch(() => {
-          NProgress.done()
-        })
+          .catch(() => {
+            NProgress.done()
+          })
+      }
     },
     createFreshEventObject() {
       const user = this.$store.state.user.user
@@ -99,5 +183,12 @@ export default {
 <style scoped>
 .field {
   margin-bottom: 24px;
+}
+.errorMessage {
+  color: tomato;
+}
+
+.error {
+  outline: 1px solid tomato;
 }
 </style>
