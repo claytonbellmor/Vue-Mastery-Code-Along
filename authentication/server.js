81574c0ecde644c71cd63b811744222ac04dffe4
Clay <claytonbellmor@gmail.com>
2020-07-13 10:04:39 -0400

    Added Form, Token, Headers
    1. Create user; accepts user's credientials (name, email, password)
    2. Vuex; Send created credentials to server; store userData in State & local storage; add token to Axios sender so we can keep accessing private data from mock db
    3. Access protected data
    
    //Server.js
    1. Imported new registerUser component and added it's path
    
    //store.js
    1. imported axios
    2. added user object to state
    3. created action called 'REGISTER' which makes a post to lh:3000/register with credientials from registerUser component form (credentials) and sets the data response to SET USER DATA as 'data'
    4. Created a state mutation called SET_USER_DATA that takes the state and userData (credentials data) adds it to the user object, adds it to the localstorage as a json string, and creates an axios header called 'authorization' with the token we created in the _server.js file
    
    // registerUser.vue View
    1. Created a form
    2. call register when submitted
    3. register calls the vuex dispatch 'REGISTER' with the name, email, password data as 'credentials'
    4. redirects to the dashboard page

--- authentication/server.js
+++ authentication/server.js
@@ -17,10 +17,13 @@ app.get('/', (req, res) => {
 })
 
 app.get('/dashboard', verifyToken, (req, res) => {
+  // Verify we have the token
   jwt.verify(req.token, 'the_secret_key', err => {
+    // If we dont: respond with 401
     if (err) {
       res.sendStatus(401)
     } else {
+      // otherwise respond with private events
       res.json({
         events: events
       })
